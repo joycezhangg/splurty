940b0a178db5e3cfb733c1efc7d381203829fa53
Joyce <joyce.zhangg@gmail.com>
2015-10-13 22:47:28 +0000

    14 - Validation for submission length.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+	validates :saying, :presence => true, :length => { :maximum => 500, :minimum => 5 }
+	validates :author, :presence =>true, :length => { :maximum => 50, :minimum => 3 }
 end
